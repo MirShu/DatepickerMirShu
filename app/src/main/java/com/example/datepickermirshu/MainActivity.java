3d9fcd660aa62fca78e24b1511a30f76eb8de6ee
SeanLim <linlin.1016@qq.com>
2022-12-26 17:06:38 +0800

    grdle 版本统一管理

--- app/src/main/java/com/example/datepickermirshu/MainActivity.java
+++ app/src/main/java/com/example/datepickermirshu/MainActivity.java
@@ -12,16 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showDialog1();
     }
 
-
-
-
-
-
-
-    public void showDialog1() {
-        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
-    }
 }
